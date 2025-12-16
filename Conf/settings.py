f1e665c70bd1773fc5da5c86aefe735e08a2b49c
muhamad <muhamadali@gmail.com>
2025-12-16 15:49:27 +0500

    Fix settings for Render + WhiteNoise

--- Conf/settings.py
+++ Conf/settings.py
@@ -1,147 +1,16 @@
-# import os
-# from pathlib import Path
-# from decouple import config
-#
-# BASE_DIR = Path(__file__).resolve().parent.parent
-#
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-k9$##c5#jg7z&1yq=4r1a(f5e-v#we&4ekh0lcmoa)dp7z)=97'
-#
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=True, cast=bool)  # Faqat bitta DEBUG qator
-#
-# # ALLOWED_HOSTS = ['nematov.uz','www.nematov.uz','127.0.0.1']
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
-#
-# INSTALLED_APPS = [
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-#     'app_pages',
-# ]
-#
-# MIDDLEWARE = [
-#     'django.middleware.security.SecurityMiddleware',
-#     'whitenoise.middleware.WhiteNoiseMiddleware',
-#     'whitenoise.middleware.WhiteNoiseMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
-#
-# ROOT_URLCONF = 'Conf.urls'
-#
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [ BASE_DIR / 'templates' ],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-#
-# WSGI_APPLICATION = 'Conf.wsgi.application'
-#
-# # ============ DATABASES KONFIGURATSIYASI ============
-# # Variant 1: SQLite (eng oddiy, ishlab chiqish uchun)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-#
-# # Variant 2: PostgreSQL (agar kerak bo'lsa)
-# # DATABASES = {
-# #     'default': {
-# #         'ENGINE': 'django.db.backends.postgresql',
-# #         'NAME': config('DB_NAME', default='mydatabase'),
-# #         'USER': config('DB_USER', default='myuser'),
-# #         'PASSWORD': config('DB_PASS', default='mypassword'),
-# #         'HOST': config('DB_HOST', default='localhost'),
-# #         'PORT': config('DB_PORT', default='5432'),
-# #     }
-# # }
-# # ====================================================
-#
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
-#
-# # CSRF_TRUSTED_ORIGINS = [
-# #     "https://nematov.uz",
-# #     "https://www.nematov.uz"
-# # ]
-#
-# ALLOWED_HOSTS = [
-#     'localhost',
-#     '127.0.0.1',
-#     '.onrender.com',
-#     'muhammadalii.uz',
-#     'www.muhammadalii.uz',
-# ]
-#
-# LANGUAGE_CODE = 'en-us'
-# TIME_ZONE = 'Asia/Tashkent'
-# USE_I18N = True
-# USE_TZ = True
-#
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),  # static papka
-# ]
-#
-#
-# STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-#
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media/'
-#
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#
-# # Resume faylini saqlash uchun
-# RESUME_PATH = 'resume/'
-#
-# RECAPTCHA_PUBLIC_KEY ="6LfzCygsAAAAAKns4sIp-FebWZpD7ZuDvUjXzvWt"
-# RECAPTCHA_SECRET_KEY = "6LfzCygsAAAAANMlWytBGCNUxZ-cceSf3n3b1h8c"
-
-
 import os
 from pathlib import Path
 from decouple import config
 
+# ================= BASE =================
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-k9$##c5#jg7z&1yq=4r1a(f5e-v#we&4ekh0lcmoa)dp7z)=97')
+# ================= SECURITY =================
+SECRET_KEY = config(
+    'SECRET_KEY',
+    default='django-insecure-k9$##c5#jg7z&1yq=4r1a(f5e-v#we&4ekh0lcmoa)dp7z)=97'
+)
+
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = [
@@ -166,7 +35,7 @@ INSTALLED_APPS = [
 # ================= MIDDLEWARE =================
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # faqat bittasi
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -175,12 +44,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# ================= URLS / WSGI =================
 ROOT_URLCONF = 'Conf.urls'
 
+WSGI_APPLICATION = 'Conf.wsgi.application'
+
+# ================= TEMPLATES =================
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates' ],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -193,8 +66,6 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Conf.wsgi.application'
-
 # ================= DATABASE =================
 DATABASES = {
     'default': {
@@ -205,22 +76,33 @@ DATABASES = {
 
 # ================= PASSWORD VALIDATORS =================
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+# ================= LANGUAGE / TIME =================
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Tashkent'
+USE_I18N = True
+USE_TZ = True
+
 # ================= STATIC FILES =================
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# WhiteNoise (ENG MUHIM QISM)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ['.map']
 
-# ================= MEDIA FILES =================
+# ================= MEDIA =================
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -228,9 +110,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 RESUME_PATH = 'resume/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ['.map']
-
+# ================= RECAPTCHA =================
 RECAPTCHA_PUBLIC_KEY = "6LfzCygsAAAAAKns4sIp-FebWZpD7ZuDvUjXzvWt"
 RECAPTCHA_SECRET_KEY = "6LfzCygsAAAAANMlWytBGCNUxZ-cceSf3n3b1h8c"
